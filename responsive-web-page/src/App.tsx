cf995cfe55e78919354a89030f0833cd2a6acf9c
JoelVRV <joelc456789@gmail.com>
2025-02-10 17:41:31 +0100

    ---

--- responsive-web-page/src/App.tsx
+++ responsive-web-page/src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
-import React from 'react';
+//import React from 'react';
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import Analytics from './components/Analytics.jsx';
